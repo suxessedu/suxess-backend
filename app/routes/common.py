9492d5be5ae6075c0e35cd38cb08cc04572ad915
Omobolaji Durojaiye <99079690+OmobolajiDurojaiye@users.noreply.github.com>
2026-01-28 17:41:39 +0100

    added new route health

--- app/routes/common.py
+++ app/routes/common.py
@@ -3,6 +3,10 @@ from app.utils.subjects import get_all_subjects
 
 common_bp = Blueprint('common', __name__)
 
+@common_bp.route('/health', methods=['GET'])
+def health_check():
+    return jsonify({"status": "ok"}), 200
+
 @common_bp.route('/subjects', methods=['GET'])
 def get_subjects():
     """
